f874d461975a5ebc2e69af691cdebfa17172a830
Ryan Elliott <rselliott99@gmail.com>
2021-02-13 04:05:10 -0500

    Add chat hover animation

--- app/src/components/Chat.jsx
+++ app/src/components/Chat.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import styles from '../styles/Chat.module.css';
-import profile from '../assets/images/profile.png'
 
 export default class Chat extends Component {
     render() {
@@ -9,15 +8,15 @@ export default class Chat extends Component {
         const text_container_styles = `${styles['text-container']}`;
         const chat_name_styles = `${styles['chat-name']} text-gray-darker font-medium`;
         const preview_styles = `${styles['message-preview']} text-gray-regular`;
-
+        
         return (
             <div className={chat_container_styles}>
-                <img src={profile} alt="" className={image_styles}/>
+                <img src={this.props.imageUrl} alt="" className={image_styles}/>
                 <div className={text_container_styles}>
                     <p className={chat_name_styles}>{this.props.name}</p>
                     <p className={preview_styles}>{this.props.message}</p>
                 </div>
             </div>
-        )
+        );
     }
 }
