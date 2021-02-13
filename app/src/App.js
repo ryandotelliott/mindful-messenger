1f141b800528e4d1c9059f91d4ea1d0330e26380
Ryan Elliott <rselliott99@gmail.com>
2021-02-13 04:05:28 -0500

    Add example chats

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import Chat from "./components/Chat";
+import ChatsContainer from './components/ChatsContainer';
 
 function App() {
+    let chats = [
+        {
+            name: "Harris Rothaermel",
+            imageUrl: "https://i.imgur.com/3LdWieK.png",
+            message: "This is an example message to show how the app looks with chats."
+        },
+        {   
+            name: "Aditya Singhal",
+            imageUrl: "https://i.imgur.com/3LdWieK.png",
+            message: "This is an example message to show how the app looks with chats."
+        },
+        {   
+            name: "Ryan Elliott",
+            imageUrl: "https://i.imgur.com/3LdWieK.png",
+            message: "This is an example message to show how the app looks with chats."
+        },
+    ]
   return (
     <div className="App">
-        <Chat name="Harris Rothaermel" message="This is an example past message!"/>
+        <ChatsContainer chats={chats} />
     </div>
   );
 }
