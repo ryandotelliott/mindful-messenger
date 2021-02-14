cc99ccd2e3eef473cb078d861b27fcd2573cca98
Ryan Elliott <rselliott99@gmail.com>
2021-02-13 23:21:58 -0500

    Fix ChatsView sizing issue

--- app/src/views/ChatsView.jsx
+++ app/src/views/ChatsView.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import GenericHeader from '../components/GenericHeader';
 import ChatsContainer from '../components/ChatsContainer';
+import styles from '../styles/ChatsView.module.css';
 
 export default class ChatsView extends Component {
     render() {
         return (
-            <div>
+            <div className={styles.container}>
                 <GenericHeader text="Chats" image="add_chat" route="/compose"/>
                 <ChatsContainer chats={this.props.chats}/>
             </div>
